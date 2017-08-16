74bc05e5f538ec829dcaaf102044d85b49075cd1
Zhao70_virtualUbuntu <qilingzhao8@gmail.com>
2017-08-16 13:27:04 +0800

    ch3

---
+++ ch3/TestString.java
@@ -0,0 +1,7 @@
+import java.lang.String;
+public class TestString{
+	public static void main(String[] args) {
+        String str = "helloworld";
+        System.out.println(str.substring(0, 5));
+	}
+}
