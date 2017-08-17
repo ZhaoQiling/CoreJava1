c7d76bf518f3ba1f1e1198f5e3e7150eba1e0acc
ZhaoQiLing <qilingzhao8@gmail.com>
2017-08-17 16:14:10 +0800

    PrintWriter

--- ch3/FileIO.java
+++ ch3/FileIO.java
@@ -8,7 +8,7 @@ public class FileIO {
     public static void main(String[] args) throws IOException, FileNotFoundException{
         Scanner in = new Scanner(Paths.get("Alice_in_Wonderland.txt"), "UTF-8");
         PrintWriter out = new PrintWriter(".\\mybook\\myAlice.txt", "UTF-8");
-        while(in.hasNextLine()){
+            while(in.hasNextLine()){
             out.print(in.nextLine());
             out.println();
         }
